5041f17dde47edaa60c42ad432d320a8432ea2f6
suchitrakr <142829963+suchitrakr@users.noreply.github.com>
2023-10-20 13:00:50 -0500

    Update LoginTests.java
    comment update

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -16,7 +16,7 @@ public class LoginTests extends BaseTest {
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        // testing
+        //testing
         String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
